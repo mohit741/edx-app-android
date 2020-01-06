1c4f69e24059b04ceeb301a03ae1104463cad911
Farhan Arshad <farhan.arshad@arbisoft.com>
2020-01-06 21:09:32 +0500

    LEARNER-3877 - Add Microsoft Authentication to Mobile Login / Registration

--- OpenEdXMobile/src/main/java/org/edx/mobile/social/ISocial.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/social/ISocial.java
@@ -2,19 +2,29 @@ package org.edx.mobile.social;
 
 import android.app.Application.ActivityLifecycleCallbacks;
 import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import com.microsoft.identity.client.exception.MsalException;
 
 import org.edx.mobile.logger.Logger;
+import org.jetbrains.annotations.NotNull;
 
 public interface ISocial extends ActivityLifecycleCallbacks {
 
-    public final Logger logger = new Logger(ISocial.class.getName());
-    
+    Logger logger = new Logger(ISocial.class.getName());
+
     void login();
     void logout();
     void setCallback(ISocial.Callback callback);
-    void onActivityResult(int requestCode, int resultCode, Intent data);
+    void onActivityResult(int requestCode, int resultCode, @Nullable Intent data);
 
-    public static interface Callback {
+    interface Callback {
         void onLogin(String accessToken);
+
+        default void onCancel() {
+        }
+
+        default void onError(@Nullable Exception exception) {
+        }
     }
 }
